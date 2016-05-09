19a24e6c168951c0daff6199cea9ca77c8da5a1c
authorbeard <msnolan@gmail.com>
2016-05-08 21:45:05 -0400

    all specs passing

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -25,6 +25,7 @@ RSpec.describe ProductsController, type: :controller do
 
       get :show, id: product.id, format: 'json'
       body = JSON.parse(response.body)["product"]
+byebug
       expect(body["name"]).to eq product.name
       expect(body["description"]).to eq product.description
       expect(body["inventory"]).to eq product.inventory
