daf28f18504d2610656654e81afee2be772bc338
Traci7822 <Tracir@gmail.com>
2016-11-15 12:07:46 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
-  resources :orders
+  resources :orders, only: [:index, :show]
   resources :invoices
   resources :products, only: [:show, :index, :new, :create]
 end
