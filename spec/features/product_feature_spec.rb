4404759bd4da1d8bb468d2d09e3723febe3e6915
Dom McKellar <dom.mckellar@gmail.com>
2016-11-18 12:23:00 -0800

    Done.

--- spec/features/product_feature_spec.rb
+++ spec/features/product_feature_spec.rb
@@ -43,6 +43,7 @@ RSpec.describe "Products", type: :feature do
       visit products_path
       expect(page).to have_content(product.name, count: 1)
       expect(page).not_to have_content product.description
+      binding.pry
       click_button "More Info"
       expect(page).to have_content product.description
       expect(page).to have_content "Sold Out"
