f65dd17d3c2ddd47b110f35eaed92d1e08feabce
Andrew <andrew.ramell@gmail.com>
2017-12-04 13:42:00 -0500

    Done.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,10 @@ class OrdersController < ApplicationController
 
   def index
     @orders = Order.all
+    respond_to do |f|
+      f.html {render :index}
+      f.json {render json: @orders}
+    end
   end
 
 end
