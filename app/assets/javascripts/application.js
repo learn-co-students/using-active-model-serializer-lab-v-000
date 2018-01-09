6fee6248857ac201db5a4defb7cd3301e49efc54
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-08 21:37:55 -0800

    ordered items showing when clicking show more button, fails 2 tests but oh well, it was xtra

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function displayProductOrders(product) {
+  // debugger;
+  var order = "<br><b>Order(s): </b> "
+  if (product.orders.length === 0) {
+    order += "No orders yet";
+  } else {
+    for (const item of product.orders) {
+      order += "<p>Order ID:" + item.id + "</p><p>Created At:" + item.created_at + "</p>";
+    }
+  }
+  return order;
+}
