dfd0db66be0b71d572dd56458f6a6754aecab7af
dbfarms <matt.hunger@gmail.com>
2018-01-17 19:47:41 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,8 +25,8 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     respond_to do |format|
+      format.json { render json: @product} #, status: 200
       format.html { render :show }
-      format.json { render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])}
     end
   end
 
