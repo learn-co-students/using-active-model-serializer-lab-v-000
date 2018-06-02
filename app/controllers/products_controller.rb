158de0a90d382dc7f3eaddaefe8e8ce81ec82152
Lizzie <lizzie.sorin@hotmail.fr>
2018-06-01 17:24:46 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,15 +3,15 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
-  end
+  # def inventory
+  #   product = Product.find(params[:id])
+  #   render plain: product.inventory > 0 ? true : false
+  # end
 
-  def description
-    product = Product.find(params[:id])
-    render plain: product.description
-  end
+  # def description
+  #   product = Product.find(params[:id])
+  #   render plain: product.description
+  # end
 
   def new
     @product = Product.new
@@ -23,11 +23,15 @@ class ProductsController < ApplicationController
   end
 
   def show
+
     @product = Product.find(params[:id])
+    # let's check
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])}
+      format.json { render json: @product}
     end
+    # render json: @product
+
   end
 
   private
