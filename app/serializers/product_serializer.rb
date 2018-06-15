cc505684b488993acc463f87bf0e67343601a05b
Chaim Reisman <chaim@Chaims-MacBook-Pro.local>
2018-06-15 13:30:58 -0400

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :description, :inventory, :name, :price, :id
-  has_many :orders
+  has_many :ordered_products
+  has_many :orders, :through => :ordered_products
 end
