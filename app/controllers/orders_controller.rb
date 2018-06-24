16ab6b1456e897152cc8fedbe92e07c4a152a13b
ecssiah <ecssiah@gmail.com>
2018-06-24 01:45:34 -0500

    Done.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,12 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
 
+
+  def show
+    @order = Order.find(params[:id])
+    
+    respond_to do |format|
+      format.json { render json: @order }
+    end
+  end
 end
