88e5896b987876975e59da0c0e76eb4e351d9187
Maverick Tesini <mavericktesini@gmail.com>
2018-08-22 08:10:40 +0000

    Done.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,15 @@ class OrdersController < ApplicationController
 
   def index
     @orders = Order.all
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @orders, each_serializer: OrderSerializer}
+    end
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    render json: @order, status: 200
   end
 
 end
