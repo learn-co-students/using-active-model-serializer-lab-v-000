77f7df24340786e79d2053dad4d60332533df98e
github username <github email address>
2018-10-08 15:50:20 -0400

    Done.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,12 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
 
+  def show
+    @order = Order.find_by(id: params[:id])
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @order}
+    end
+  end
+
 end
