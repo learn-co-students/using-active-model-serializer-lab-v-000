9a47dec999c448d6dceee05263908fc15e80d992
Mallory Feuer <mallory.feuer@gmail.com>
2018-11-13 20:31:47 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,16 +3,6 @@
     @products = Product.all
   end
 
-  def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
-  end
-
-  def description
-    product = Product.find(params[:id])
-    render plain: product.description
-  end
-
   def new
     @product = Product.new
   end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,8 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :price, :inventory
+<<<<<<< HEAD
   has_many :orders, through: :ordered_products
+=======
+  belongs_to :order
+>>>>>>> a36e4b4a22ff9222caa199f741bde07918efdfed
 end
