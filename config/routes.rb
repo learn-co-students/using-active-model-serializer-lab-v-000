9229bb95d9d5391f31db9ac291df2706f25680bf
FKRauber <fabienne.rauber@gmail.com>
2019-02-13 17:22:02 -0500

    completed lab per instructions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:show, :index, :new, :create]
-  get '/products/:id/description', to: 'products#description'
-  get '/products/:id/inventory', to: 'products#inventory'
+  resources :products, only: [:index, :show, :new, :create]
+
 end
