c0031dd2741ec561d2b74a8ff6d79375ec04f81c
harleyharl <harleymteden@gmail.com>
2019-05-22 10:38:46 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,10 +23,11 @@ class ProductsController < ApplicationController
   end
 
   def show
+    # binding.pry
     @product = Product.find(params[:id])
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])}
+      format.json { render json: @product, status: 200}
     end
   end
 
