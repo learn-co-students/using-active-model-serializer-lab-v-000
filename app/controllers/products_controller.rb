f86b77cf7ff7a271aef9daebe83fc59e46fb40db
Guess <guess@Guesss-iMac.local>
2019-05-22 16:19:57 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,10 +8,10 @@ class ProductsController < ApplicationController
     render plain: product.inventory > 0 ? true : false
   end
 
-  def description
-    product = Product.find(params[:id])
-    render plain: product.description
-  end
+  #def description
+    #product = Product.find(params[:id])
+    #render plain: product.description
+  #end
 
   def new
     @product = Product.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:show, :index, :new, :create]
-  get '/products/:id/description', to: 'products#description'
-  get '/products/:id/inventory', to: 'products#inventory'
+  resources :products, only: [:index, :show, :new, :create]
 end
