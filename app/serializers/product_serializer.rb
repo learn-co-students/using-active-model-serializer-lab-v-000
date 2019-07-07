9bbb548763ed92d5252fbdd2d3602e6caa9f4209
github username <github email address>
2019-07-07 08:13:40 -0400

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :price, :inventory
+  attributes :id, :name, :description, :price, :inventory, :created_at
 end
